bac07cc9db38c34495bdaba095874f7f9be3d0a8
EvilAv <andrey.vookolov85@gmail.com>
2024-03-07 16:09:56 +0300

    small fixes for deploy

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import { makeFinalPage } from "./parser.js";
 
-const PORT = 3030;
-const HOST = "localhost";
+const DEV_PORT = 3030;
+const BUILD_PORT = 8080;
+
+const PORT =
+    process.argv[2] && process.argv[2] === "-b" ? BUILD_PORT : DEV_PORT;
+const HOST = "127.0.0.1";
 
 const app = express();
-app.use(express.static('src'));
+app.use(express.static("src"));
 
 app.get("/*", (request, response) => {
-    response.setHeader("Content-Type", "text/html")
+    response.setHeader("Content-Type", "text/html");
     response.send(makeFinalPage());
 });
 
