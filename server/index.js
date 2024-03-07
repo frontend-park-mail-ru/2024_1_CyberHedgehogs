ce7f9f55f778f44001fdb56497c1eba617a52f87
EvilAv <andrey.vookolov85@gmail.com>
2024-03-07 16:45:55 +0300

    more fixes for deploy

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 import express from "express";
+import os from "os";
 import { makeFinalPage } from "./parser.js";
 
 const DEV_PORT = 3030;
 const BUILD_PORT = 8080;
 
+const DEV_HOST = "localhost";
+const BUILD_HOST = Object.values(os.networkInterfaces())
+    .flat()
+    .find((iface) => iface.family === "IPv4" && !iface.internal).address;
+
 const PORT =
     process.argv[2] && process.argv[2] === "-b" ? BUILD_PORT : DEV_PORT;
-const HOST = "127.0.0.1";
+const HOST =
+    process.argv[2] && process.argv[2] === "-b" ? BUILD_HOST : DEV_HOST;
 
 const app = express();
 app.use(express.static("src"));
